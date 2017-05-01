9b4d30e3a0575f23b11b6c209c81673df4fb3c69
locmark <kkraczyna@gmail.com>
2017-05-01 10:42:21 +0200

    buttons - creating, attaching actions

---
+++ main.cpp
@@ -0,0 +1,144 @@
+#include <windows.h>
+#include <iostream>
+#include <string>
+
+char* NazwaKlasy = (char*)"Klasa Okienka";
+MSG Komunikat;
+
+LRESULT CALLBACK WndProc( HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam );
+
+// class document_t {
+// public:
+//   document_t() {}
+// };
+
+class window_t {
+  HWND hwnd;
+  HWND hwndButton;
+  HINSTANCE hInstance;
+
+  void(*buttonActions[])();
+public:
+  // document_t* document;
+  init (HINSTANCE hInstance_to_save, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    hInstance = hInstance_to_save;
+    // WYPEŁNIANIE STRUKTURY
+    WNDCLASSEX wc;
+
+    wc.cbSize = sizeof( WNDCLASSEX );
+    wc.style = 0;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon( NULL, IDI_APPLICATION );
+    wc.hCursor = LoadCursor( NULL, IDC_ARROW );
+    wc.hbrBackground =( HBRUSH )( COLOR_WINDOW + 1 );
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = NazwaKlasy;
+    wc.hIconSm = LoadIcon( NULL, IDI_APPLICATION );
+
+    // REJESTROWANIE KLASY OKNA
+    if( !RegisterClassEx( & wc ) )
+    {
+        MessageBox( NULL, "Wysoka Komisja odmawia rejestracji tego okna!", "Niestety...",
+        MB_ICONEXCLAMATION | MB_OK );
+        return 1;
+    }
+
+    // TWORZENIE OKNA
+
+
+    hwnd = CreateWindowEx( WS_EX_CLIENTEDGE, NazwaKlasy, "Oto okienko", WS_OVERLAPPEDWINDOW,
+    CW_USEDEFAULT, CW_USEDEFAULT, 800, 480, NULL, NULL, hInstance, NULL );
+
+    if( hwnd == NULL )
+    {
+        MessageBox( NULL, "Okno odmówiło przyjścia na świat!", "Ale kicha...", MB_ICONEXCLAMATION );
+        return 1;
+    }
+
+    ShowWindow( hwnd, nCmdShow ); // Pokaż okienko...
+    UpdateWindow( hwnd );
+  }
+
+  loop () {
+    while( GetMessage( & Komunikat, NULL, 0, 0 ) )
+    {
+        TranslateMessage( & Komunikat );
+        DispatchMessage( & Komunikat );
+    }
+  }
+
+  events ( HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam ) {
+    int wmId, wmEvent;
+    switch( msg ) {
+      case WM_COMMAND:
+        wmId = LOWORD(wParam);
+        wmEvent = HIWORD(wParam);
+        buttonActions[wmId]();
+        break;
+
+      // case WM_PAINT:
+      //   PAINTSTRUCT ps;
+      //   HDC hdc = BeginPaint(hwnd, &ps);
+      //   // TODO: Add any drawing code that uses hdc here...
+      //   EndPaint(hwnd, &ps);
+      //   break;
+      //
+      case WM_CLOSE:
+        DestroyWindow( hwnd );
+        break;
+
+      case WM_DESTROY:
+        PostQuitMessage( 0 );
+        break;
+
+      default:
+        return DefWindowProc( hwnd, msg, wParam, lParam );
+    }
+  }
+
+  addButton (std::string text, unsigned int id, unsigned int x, unsigned int y, unsigned int width, unsigned int height) {
+    hwndButton = CreateWindow(TEXT("button"),                      // The class name required is button
+		TEXT(text.c_str()),                  // the caption of the button
+		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,  // the styles
+		x, y,                                  // the left and top co-ordinates
+		width, height,                              // width and height
+		hwnd,                                 // parent window handle
+		(HMENU)id,                   // the ID of your button
+		hInstance,                            // the instance of your application
+		NULL);                               // extra bits you dont really need
+
+    attachActionToButton(id, []()->void{});
+    // UpdateWindow(hwnd);
+  }
+
+  attachActionToButton (unsigned int id, void(*function)()) {
+    buttonActions[id] = function;
+  }
+};
+
+
+
+// document_t document;
+window_t window;
+
+int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow )
+{
+  window.init(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
+  window.addButton("test_button", 2100, 200, 300, 100, 50);
+  window.addButton("test_button2", 2101, 200, 250, 100, 50);
+  window.attachActionToButton(2100, []()->void{
+    std::cout << "dupa\n";
+  });
+  window.attachActionToButton(2101, []()->void{
+    std::cout << "dupa2\n";
+  });
+  window.loop();
+}
+
+
+LRESULT CALLBACK WndProc( HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam ) {
+    window.events(hwnd, msg, wParam, lParam);
+}
