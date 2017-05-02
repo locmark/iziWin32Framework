cb2d3f03cdb10744e18828fd7128a83cd0e170b3
locmark <kkraczyna@gmail.com>
2017-05-02 19:33:22 +0200

    Interval (timer)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
 #include <iostream>
 #include <string>
+#include <vector>
 
 char* NazwaKlasy = (char*)"Klasa Okienka";
 MSG Komunikat;
@@ -113,10 +114,37 @@ public:
   }
 };
 
+
+class interval_t {
+  HWND localHwnd;
+  unsigned int _id;
+  void(*action)();
+public:
+  Create (unsigned int id, unsigned int interval) {
+    _id = id;
+    SetTimer(hwnd, id, interval, 0);
+  }
+
+  Stop () {
+    KillTimer(hwnd, _id);
+  }
+
+  SetAction (void(*function)()) {
+    action = function;
+  }
+
+  Action () {
+    action();
+  }
+};
+
+
 class window_t {
+  // std::vector<button_t> buttonList;  // not working :(
   button_t buttonList[1000];
   textbox_t textboxList[1000];
   radiobutton_t radiobuttonList[1000];
+  interval_t intervalList[1000];
 
   void(*buttonActions[])();
 public:
@@ -179,6 +207,10 @@ public:
         actions[wmId]();
         break;
 
+      case WM_TIMER:
+        intervalList[wParam].Action();
+        break;
+
       // case WM_PAINT:
       //   PAINTSTRUCT ps;
       //   HDC hdc = BeginPaint(hwnd, &ps);
@@ -202,6 +234,7 @@ public:
   button_t& button (unsigned int id) { return buttonList[id]; }
   textbox_t& textbox (unsigned int id) { return textboxList[id]; }
   radiobutton_t& radiobutton (unsigned int id) { return radiobuttonList[id]; }
+  interval_t& interval (unsigned int id) { return intervalList[id]; }
 };
 
 
@@ -218,6 +251,17 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin
   window.button(2100).OnClick ([]()->void{
     std::cout << "test132\n";
     window.textbox(200).SetText("reset :)");
+  });
+
+  window.interval(2).Create(2, 500);
+  window.interval(2).SetAction([]()->void{
+    static int times = 0;
+    times++;
+    std::cout << "timer works :P " << times << "\n";
+
+    if (times >= 10) {
+      window.interval(2).Stop();
+    }
   });
 
   window.Loop ();
